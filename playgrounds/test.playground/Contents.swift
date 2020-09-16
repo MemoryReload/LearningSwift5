8304ca26f25fa38024bb2bdb3615f0c47913e16e
ping.he <ping.he@kuwo.cn>
2020-09-16 20:36:30 +0800

    add a new test playground

---
+++ playgrounds/test.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let str = "a👿b🇩🇪c"
+let r1 = str.range(of: "🇩🇪")!
+
+// String range to NSRange:
+let n1 = NSRange(r1, in: str)
+print((str as NSString).substring(with: n1)) // 🇩🇪
+// NSRange back to String range:
+let r2 = Range(n1, in: str)!
+print(str.substring(with: r2)) //
+
+print(str[r2.lowerBound..<r2.upperBound])
