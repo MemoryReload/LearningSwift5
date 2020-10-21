6ef75a69d087966efd449c6b2aeab44b11404f2e
ping.he <ping.he@kuwo.cn>
2020-10-21 19:39:17 +0800

    闭包更多练习代码

--- playgrounds/closure.playground/Contents.swift
+++ playgrounds/closure.playground/Contents.swift
@@ -1,8 +1,9 @@
+//take closure as parameters
 func travel(to place: String, action: (String, Int) -> String){
     print("I'm traveling to \(place)")
     let desc = action("car",60)
     print(desc);
-    print("I'm arrived!")
+    print("I'm arrived!\n")
 }
 
 travel(to: "London") { (method: String, speed: Int) -> String in
@@ -18,3 +19,35 @@ travel(to: "Willson"){ (method: String, speed: Int) -> String in
 travel(to: "Irish"){
     "I'm now traveling by \($0) with speed \($1) km/h"
 }
+
+//return closure as function return value
+func makeTravelAction1(to place: String) -> (String, String) -> Void {
+    return { (person: String, method: String) -> Void in
+        print("I'm traveling to \(place) with \(person) by \(method).\n")
+    }
+}
+
+func makeTravelAction2(to place: String) -> (String, String) -> Void {
+    return {
+        print("I'm traveling to \(place) with \($0) by \($1).\n")
+    }
+}
+
+var travelAction = makeTravelAction1(to: "China")
+travelAction("Jone","ship")
+travelAction = makeTravelAction2(to: "Russia")
+travelAction("Jacky","plane")
+
+func makeIncreaseAction(from start: Int) -> (Int) -> Void {
+    var counter = start
+    return {
+        counter += $0
+        print("result: \(counter)")
+    }
+}
+
+let increase = makeIncreaseAction(from: 0)
+increase(1)
+increase(1)
+increase(1)
+
