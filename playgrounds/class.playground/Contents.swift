e17c8bf252611cdc8623c0e7037a590e1759273b
ping.he <ping.he@kuwo.cn>
2020-10-28 13:59:50 +0800

    add more stuff for class

--- playgrounds/class.playground/Contents.swift
+++ playgrounds/class.playground/Contents.swift
@@ -2,11 +2,16 @@ class Student
 {
     var name: String = ""
     var age: Int = 0
+    /*
+     When all properties have default values, we don't need to define a customized initialization method.
+     The compiler will make a default one `init()` for us.
+     */
 }
 
 class UniversityStudent: Student
 {
     var annualFees: Int
+    
     init(fees:Int) {
         annualFees = 0 //this one is fucking needed, because initialization phase 1.
         super.init()
@@ -17,6 +22,7 @@ class UniversityStudent: Student
 class UniversityStudent2: Student
 {
     var annualFees: Int = 0 //this keeps initialization phase 1 damn good
+    
     init(fees:Int) {
 //        annualFees = 0 //now this could  be left out.
         super.init()
@@ -26,17 +32,38 @@ class UniversityStudent2: Student
 
 class UniversityStudent3: Student
 {
-    var annualFees: Int
+    private(set) var annualFees: Int
+    
     init(fees:Int) {
-        annualFees = fees //looks strange, but all in one.
+        self.annualFees = fees //looks strange, but all in one. the same to `annualFees = fees`
+//        self.name = "annoymus" //This is not allowed, interesting! because `self` is only available after phase 2. but look line above, be careful, that's phase 1, the `self.annualFees` really mean `annualFees`
         super.init()
+        self.name = "annoymus"
+    }
+    
+    func changeFee(to fee: Int) {
+        annualFees = fee
     }
     
     convenience override init() {
         self.init(fees:0)
     }
+    
+    deinit {
+        print("instance destroyed")
+    }
 }
 
 let s = UniversityStudent3()
-print("the student fee is \(s.annualFees)")
+//s.annualFees = 80 //This will throw an error, because the setter is private.
+s.changeFee(to: 80)
+print("the student \(s.name)'s annual fee is \(s.annualFees)")
 
+final class FinalClass {
+    
+}
+
+//final class can not be inherited any more
+//class TestClass: FinalClass {
+//
+//}
