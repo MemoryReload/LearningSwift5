c325f0b2ae535f6c83adca6625d649ed6c07e22a
ping.he <ping.he@kuwo.cn>
2020-11-23 20:17:26 +0800

    add Swift UI stuff

---
+++ SwiftUI/idine/idine/idineApp.swift
@@ -0,0 +1,17 @@
+//
+//  idineApp.swift
+//  idine
+//
+//  Created by 何平 on 2020/11/23.
+//
+
+import SwiftUI
+
+@main
+struct idineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
