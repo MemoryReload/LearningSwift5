33195d1951503e632147410ee31a045273c3dfab
ping.he <ping.he@kuwo.cn>
2020-11-23 21:09:00 +0800

    add the detail image view

---
+++ Classic/project1/project1/DetailController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailController.swift
+//  project1
+//
+//  Created by 何平 on 2020/11/23.
+//
+
+import UIKit
+
+class DetailController : UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        
+    }
+}
