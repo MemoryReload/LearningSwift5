acbbd24de92b9a7fa80db1b6afcd2fcf1e59f734
ping.he <ping.he@kuwo.cn>
2020-11-23 14:33:33 +0800

    添加Classic代码

---
+++ Classic/project1/project1/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by 何平 on 2020/11/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print("\(pictures)")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PictureCell", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+}
+
