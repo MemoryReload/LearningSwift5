3093d1d4e13832d825fd72f3451c2c76f285fdb5
ping.he <ping.he@kuwo.cn>
2020-11-26 15:26:41 +0800

    add project2

---
+++ Classic/project2/project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by 何平 on 2020/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
