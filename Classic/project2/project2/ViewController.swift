4c5d6e0d5799aef0350272eeebc7ee571bf6a5fb
ping.he <ping.he@kuwo.cn>
2020-11-26 17:15:19 +0800

    add those dumb bunttons

--- Classic/project2/project2/ViewController.swift
+++ Classic/project2/project2/ViewController.swift
@@ -12,11 +12,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var btn2: UIButton!
     @IBOutlet weak var btn3: UIButton!
     
+    var countries = [String]()
+    var btns = [UIButton]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countries += ["estonia.png", "france.png", "germany.png", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        btns += [btn1,btn2,btn3]
+        askQuestion()
     }
-
-
+    
+    func askQuestion() {
+        for index in 0..<3 {
+            let btn = btns[index]
+            let image = UIImage(named: countries[index])
+            btn.setImage(image, for: .normal)
+            btn.layer.borderWidth = 1
+            btn.layer.borderColor = UIColor.lightGray.cgColor
+        }
+    }
 }
 
