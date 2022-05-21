6aa50c2b11c7ddf47669f14bf047eca09474819b
MemoryReload <heping_tsdwx@163.com>
2022-05-22 00:08:09 +0800

    the memorize porject

---
+++ SwiftUI/Stanford_cs193p/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by HePing on 2022/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
